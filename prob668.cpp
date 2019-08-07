ad8caf24fc4835fe9f761b98726b16fb80580b93
om49040 <51122265+om49040@users.noreply.github.com>
2019-08-07 19:57:50 +0530

    Create prob668.cpp

---
+++ prob668.cpp
@@ -0,0 +1,33 @@
+#include<math.h>
+#include<iostream>
+//program for square root smooth num
+using namespace std;
+int main()
+{
+// works for integer values of n
+ long long int n,temp1,temp=1,cnt=1;
+ int i=2,j;long double rt;
+ cout<<"Enter a number above 0 till which we have to find out the number of sqrt smooth num\n";
+ cin>>n;
+ while(i<=n)
+ {
+  rt=sqrt(i);
+  temp1=i;
+  for(j=2;j<rt;j+=1)
+   {
+    while(temp1%j==0)
+     {
+      temp=temp*j;//multiplies prime factors
+      temp1=temp1/j;
+     }
+   }//finds the prime factors and multiplies
+   if(i==temp)
+    {cnt=cnt+1;cout<<"\n"<<i<<"\n";}//counts the numer of smooth sqrt numbers
+   ++i;
+   temp=1;
+ }
+ cout<<"There are "<<cnt<<" number of square root smooth numbers in the range";
+ return 0;
+}
+
+   
