50e74b683ad5dd7f5ce89b726fe85ec14a649879
om49040 <51122265+om49040@users.noreply.github.com>
2019-08-07 20:03:52 +0530

    Create prob6.cpp

---
+++ prob6.cpp
@@ -0,0 +1,11 @@
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+ int s1,s2;
+ s1=pow((100*101)/2,2);
+ s2=(100)*(201)*(101)/6;
+ cout<<"The difference between square sum and sum square:"<<(s2-s1);
+ return 0;
+}
