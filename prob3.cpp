d099b52cf5ad2475aaacf23aadbe9662ea8a737c
om49040 <51122265+om49040@users.noreply.github.com>
2019-08-23 22:42:09 +0530

    Update prob3.cpp

--- prob3.cpp
+++ prob3.cpp
@@ -3,27 +3,30 @@
 using namespace std;
 int main()
 {
- int flag=0;
- long long int n,mp=0;
- cout<<"Enter the umber whose prime factor is to be found......";
- cin>>n;
- long double h=sqrt(n);
- for(int i=3;i<=h;i+=2)
-  { 
-    if(n%i==0)
-     { 
-       mp=i;flag=1;
-     }
-  }
- if(flag==1)
+ int flag=0,j,i;
+ long long int n=600851475143;
+ int h=(int)sqrt(n),t;
+ for( i=h;i>=3;--i)
+ { 
+  if(n%i==0)
   {
-   cout<<"The largest prime factor of the number="<<mp;
-   return 0;
+  	 j=3;t=(int)sqrt(i);flag=0;
+  	 while(j<t)
+  	 {
+  	 	 if(i%j==0)
+  	 	{
+  	 	 	flag=1;break;
+		}
+		j=j+2;
+	 }
+	 if(flag==1)
+	 continue;
+	 else
+	 break;
+	  
   }
- else
-   {
-    cout<<"The number "<<n<<" is a prime number";
-   }
+ }
+ cout<<"The largest prime number is:"<<i;
  return 0;
 }
   
