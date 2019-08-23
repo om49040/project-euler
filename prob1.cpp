fd612104381c325a480dd08b22a6b6e84d6d79e3
om49040 <51122265+om49040@users.noreply.github.com>
2019-08-23 22:36:38 +0530

    Update prob1.cpp

--- prob1.cpp
+++ prob1.cpp
@@ -1,19 +1,22 @@
 #include<iostream>
 using namespace std;
-//program for only 1000 numbers
 int main()
 {
- int i,sum=0;
- sum=(110/2)*(3 + 3*109*3);// sum for all the multiples of three
- // sum for the multiples of 5
- for(i=5;i<1000;i+=5)
-  {
-   if(i%15==0)
-    continue;
-   sum+=i;
-  }
- cout<<"\n Sum of multiples of 3 and 5="<<sum;
- return 0;
-}
+	 long int i,j,sum=0,flag=0;
+	for(i=3,j=5;i<1000;i=i+3,j=j+5)
+	{   
+	    if(j>=1000||flag==1)
+	    {j=0;flag=1;
+		}
+	 	if(i%5==0)
+	 	{
+	 		sum=sum+j;
+	 		continue;
+		}
+		else
+		sum=sum+i+j;
+		}
+		cout<<sum;
+	 }
 
  
