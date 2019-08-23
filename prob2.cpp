dd7b532d48fb755fa31401e76a29824338fb0f90
om49040 <51122265+om49040@users.noreply.github.com>
2019-08-23 22:39:07 +0530

    Update prob2.cpp

--- prob2.cpp
+++ prob2.cpp
@@ -2,24 +2,22 @@
 using namespace std;
 int main()
 {
- long int n;
- cout<<"Enter the Number till which we will add the fibbonacci\n";
- cin>>n;
- if(n<2)
- return 0;
+ long int n=4000000;
  long int a=0,b=2,sum=0,c=0;
  while(b<n)
   {
    c=4*b + a;//next fibbonaci even number
    if(c>n)
-    break;
+   break;
    a=b;
-   b=sum;
-   sum+=c;
+   b=c;
+   sum=sum+c;
   }
-  sum+=2;
- cout<<"Sum of even fibbonacci numbers upto "<<n<<" = "<<sum;
+  sum=sum+2;
+  cout<<"Sum of even fibbonacci numbers upto "<<n<<" = "<<sum;
  return 0;
 } 
+  
+ 
   
  
