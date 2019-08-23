19c58b8729fc881437f038bced316c1fd64851e9
om49040 <51122265+om49040@users.noreply.github.com>
2019-08-24 01:19:36 +0530

    Update prob6.cpp

--- prob6.cpp
+++ prob6.cpp
@@ -6,6 +6,6 @@ int main()
  int s1,s2;
  s1=pow((100*101)/2,2);
  s2=(100)*(201)*(101)/6;
- cout<<"The difference between square sum and sum square:"<<(s1-s2);
+ cout<<(s1-s2);
  return 0;
-}
+}//25164150
